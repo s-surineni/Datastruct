d3f4c0e388e2d4c8f28c87afbd13661cba773977
mc13mt49 <mc13mt49@linux-ac73.site>
2013-12-10 14:42:48 +0530

    first commit

---
+++ src/datastructs/Datastructs.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructs;
+
+/**
+ *
+ * @author mc13mt49
+ */
+public class Datastructs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/datastructs/Linkdlist.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructs;
+
+/**
+ *
+ * @author mc13mt49
+ */
+public class Linkdlist {
+    
+}
