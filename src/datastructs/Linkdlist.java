ce945ee9cb433f1e692f5806f97535e6612f00ac
umaram <umaram@supercom>
2013-12-11 08:31:01 +0530

    has next error corrected

--- src/datastructs/Linkdlist.java
+++ src/datastructs/Linkdlist.java
@@ -36,90 +36,6 @@ public class Linkdlist {
     void init(){
         head=tail=next=null;
     }
-        
-    
-    
-    
-    
-    void insert(){
-        int lc;
-        
-        int data;
-        System.out.println("enter a value to insert ");
-        String ip;
-        
-        while(true){
-            System.out.println("enter e to exit");
-            ip=s1.nextLine();
-            
-            if(ip.equals("e"))
-                break;
-            
-            data=Integer.parseInt(ip);
-            System.out.println("value in data "+data);
-            addToLL(data);
-            System.out.println("enter a value to insert ");
-        }
-    }
-    
-    void insertAt(){
-        System.out.println("insert the value to enter");
-        int val=s1.nextInt();
-        System.out.println("instert the position");
-        
-        int pos=s1.nextInt();
-        int count=1;
-        int tempd;
-        temp=head;
-        if(pos==1){
-            temp=new Linkdlist(val, null);
-        }
-        for( ;temp.next!=null && count<pos;count++){
-            temp=temp.next;
-        }
-        tempd=temp.data;
-        temp.data=val;
-        for( ; temp.next!=null; ){
-            temp=temp.next;
-            temp.data=temp.data+tempd;
-            tempd=temp.data-tempd;
-            temp.data=temp.data-tempd;
-            
-        }
-        
-        temp.next=new Linkdlist();
-        temp=temp.next;
-        temp.data=tempd;
-        DisplayLL();
-        
-    }
-    
-    void addToLL(int data){
-        if(head==null){
-            head=new Linkdlist();
-            head.data=data;
-            head.next=null;
-            tail=head;
-        }
-        else{
-            tail.next=new Linkdlist();
-            tail=tail.next;
-            tail.data=data;
-            tail.next=null;
-        }
-        DisplayLL();
-    }
-    
-    void DisplayLL(){
-        System.out.println("Values in the list");
-        temp=head;
-        while(temp!=null){
-            System.out.println(" "+temp.data+" ");
-            temp=temp.next;
-        }
-        System.out.println();
-        System.out.println();
-    }
     
     void giveChoice(){
         
@@ -157,6 +73,92 @@ public class Linkdlist {
         }
         
     }
+        
+        
+    void insert(){
+        int lc;
+        
+        int data;
+        System.out.println("enter a value to insert ");
+        String ip;
+        
+        while(true){
+            System.out.println("enter e to exit");
+            ip=s1.nextLine();
+            
+            if(ip.equals("e"))
+                break;
+            
+            data=Integer.parseInt(ip);
+            System.out.println("value in data "+data);
+            addToLL(data);
+            System.out.println("enter a value to insert ");
+        }
+    }
+    
+    void insertAt(){
+        System.out.println("insert the value to enter");
+        int val=Integer.parseInt(s1.nextLine());
+        System.out.println("instert the position");
+        
+        int pos=Integer.parseInt(s1.nextLine());
+        int count=1;
+        int tempd;
+        //temp=head;
+        if(pos==1){
+            head=new Linkdlist(val, null);
+        }
+        else{
+            temp=head;
+            for( ;temp.next!=null && count<pos;count++){
+                temp=temp.next;
+                }
+            tempd=temp.data;
+            temp.data=val;
+            for( ; temp.next!=null; ){
+                temp=temp.next;
+                temp.data=temp.data+tempd;
+                tempd=temp.data-tempd;
+                temp.data=temp.data-tempd;
+            
+                }
+        
+            temp.next=new Linkdlist();
+            temp=temp.next;
+            temp.data=tempd;
+            }
+        DisplayLL();
+        
+    }
+    
+    void addToLL(int data){
+        if(head==null){
+            head=new Linkdlist();
+            head.data=data;
+            head.next=null;
+            tail=head;
+        }
+        else{
+            tail.next=new Linkdlist();
+            tail=tail.next;
+            tail.data=data;
+            tail.next=null;
+        }
+        DisplayLL();
+    }
+    
+    void DisplayLL(){
+        System.out.println("Values in the list");
+        temp=head;
+        while(temp!=null){
+            System.out.println(" "+temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+        System.out.println();
+    }
+    
+    
     
     
     
